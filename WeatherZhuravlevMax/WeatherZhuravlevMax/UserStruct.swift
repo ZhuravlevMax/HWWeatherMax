82170952ce156014e71f72ed6b98f7966c309906
ZhuravlevMax <l9sik3@gmail.com>
2022-06-19 09:52:46 +0300

    created WeatherStruct; created request for json

---
+++ WeatherZhuravlevMax/WeatherZhuravlevMax/UserStruct.swift
@@ -0,0 +1,86 @@
+//
+//  UserStruct.swift
+//  WeatherZhuravlevMax
+//
+//  Created by Максим Журавлев on 18.06.22.
+//
+
+import Foundation
+
+struct Weather: Codable {
+    
+    struct Coord: Codable {
+        var lon: Decimal?
+        var lat: Decimal?
+    }
+
+    let coord: Coord
+    
+    let weather: [AboutWeather]
+    
+    struct AboutWeather: Codable {
+        var id: Int?
+        var main: String?
+        var description: String?
+        var icon: String?
+    }
+    
+    let base: String?
+    
+    struct Main: Codable {
+        var temp: Decimal?
+        var feelsLike: Decimal?
+        var tempMin: Decimal?
+        var tempMax: Decimal?
+        var pressure: Int?
+        var humidity: Int?
+        var seaLevel: Int?
+        var grndLevel: Int?
+        
+        enum CodingKeys: String, CodingKey {
+            case feelsLike = "feels_like"
+            case tempMin = "temp_min"
+            case tempMax = "temp_max"
+            case seaLevel = "sea_level"
+            case grndLevel = "grnd_level"
+            case temp, pressure, humidity
+            
+        }
+    }
+    
+    let main: Main
+    
+    let visibility: Int?
+    
+    struct Wind: Codable {
+        var speed: Decimal?
+        var deg: Int?
+        var gust: Decimal?
+    }
+    
+    let wind: Wind
+    
+    struct Clouds: Codable {
+        var all: Int?
+    }
+    
+    let clouds: Clouds
+    
+    let dt: Int?
+    
+    struct Sys: Codable {
+        var type: Int?
+        var id: Int?
+        var country: String?
+        var sunrise: Int?
+        var sunset: Int?
+    }
+    
+    let sys: Sys
+    let timezone: Int?
+    let id: Int?
+    let name: String?
+    let cod: Int?
+    
+}
+
--- WeatherZhuravlevMax/WeatherZhuravlevMax/ViewController.swift
+++ WeatherZhuravlevMax/WeatherZhuravlevMax/ViewController.swift
@@ -11,7 +11,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        //url по которому будем получать данные
+        if let url = URL(string:"https://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=e3d520a7f75cff3164067d89df3e34a8") {
+            
+            //Создаю реквест
+            var urlRequest = URLRequest(url: url)
+            
+            //Указываю тип запроса GET, т.к. получаю данные с сайта
+            urlRequest.httpMethod = "GET"
+            
+            //Указываю тип контента json
+            urlRequest.setValue("application/json", forHTTPHeaderField: "content-Type")
+            
+            let dataTask = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+                
+                if let response = response {
+                    print(response)
+                }
+                
+                if let data = data {
+                    let weather = try! JSONDecoder().decode(Weather.self, from: data)
+                    print(weather)
+                }
+                
+                if let error = error {
+                    print(error)
+                }
+            }
+            dataTask.resume()
+        }
     }
 
 
