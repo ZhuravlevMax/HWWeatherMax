78065b69760828c5b6fa003e19270fad8b3eecc8
ZhuravlevMax <l9sik3@gmail.com>
2022-07-06 12:52:31 +0300

    removed unnecessary variables

--- WeatherZhuravlevMax/WeatherZhuravlevMax/Int+Extension.swift
+++ WeatherZhuravlevMax/WeatherZhuravlevMax/Int+Extension.swift
@@ -12,16 +12,13 @@ extension Int {
         let date = Date(timeIntervalSince1970: TimeInterval(self))
         let dayTimePeriodFormatter = DateFormatter()
         dayTimePeriodFormatter.dateFormat = format
-        let dateString = dayTimePeriodFormatter.string(from: date as Date)
-        return dateString
+        return dayTimePeriodFormatter.string(from: date as Date)
     }
     
     //Форматирование Int в DateComponents
     func decoderIntToDate() -> DateComponents {
-        
         let timeInterval = TimeInterval(self)
         let myNSDate = Date(timeIntervalSince1970: timeInterval)
-        let comps = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: myNSDate)
-        return comps
+        return Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: myNSDate)
     }
 }
